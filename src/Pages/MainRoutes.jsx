191813de773a59b5226122f59f54b12cc627ea0d
chetan791 <chetanchauhan795@gmail.com>
2023-10-01 17:17:35 +0530

    fixed routing

--- src/Pages/MainRoutes.jsx
+++ src/Pages/MainRoutes.jsx
@@ -1,18 +1,17 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import ProductDetails from '../Components/Product_details'
-import LoginSignup from './LoginSignup'
-import Home from './Home'
+// import React from 'react'
+import { Route, Routes } from "react-router-dom";
+import ProductDetails from "../Components/Product_details";
+import LoginSignup from "./LoginSignup";
+import Home from "./Home";
 
 function MainRoutes() {
   return (
     <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/product_details/:id' element={<ProductDetails/>}/>
-      <Route path='/login' element={<LoginSignup/>}/>
-
+      <Route path="/" element={<Home />} />
+      <Route path="/product_details/:id" element={<ProductDetails />} />
+      <Route path="/login" element={<LoginSignup />} />
     </Routes>
-  )
+  );
 }
 
-export default MainRoutes
+export default MainRoutes;
