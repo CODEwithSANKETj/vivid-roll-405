d59f7f226249bba074c5188a4f829053efc7b426
chetan791 <chetanchauhan795@gmail.com>
2023-10-02 04:21:46 +0530

    payment page finished

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,13 @@ import Navbar from "./Components/Navbar";
 // import Home from "./Pages/Home";
 
 import MainRoutes from "./Pages/MainRoutes";
-import { Paymentpage } from "./Pages/PaymentPage";
 // import Product_list from "./Pages/Product_list";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      {/* <MainRoutes /> */}
-      <Paymentpage />
+      <MainRoutes />
     </div>
   );
 }
