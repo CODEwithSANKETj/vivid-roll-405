35bd3723382596afd47a516b26ebc331b55909db
ak1code <akshay.ramoliya9@gmail.com>
2023-10-02 19:05:47 +0530

    push

--- src/Components/Service_cart.jsx
+++ src/Components/Service_cart.jsx
@@ -8,14 +8,49 @@ import { Card, CardHeader, CardBody,Image,Stack,Heading,Text,Button, CardFooter,
   ModalBody,
   ModalCloseButton,useDisclosure,Input,FormControl,FormLabel } from '@chakra-ui/react'
 import "../CSS/service.css";
+import axios from 'axios';
+import { useSelector } from 'react-redux';
 
 
 const Service_cart = ({title,id,image,details,price}) => {
   
-  const { isOpen, onOpen, onClose } = useDisclosure()
+
+  const token=useSelector((state)=>state.auth);
+  // const cartData = useSelector((store)=>store.cart);
+  console.log(token)
+  
+
+  const { isOpen, onOpen, onClose,  } = useDisclosure()
 
   const initialRef = React.useRef(null)
   const finalRef = React.useRef(null)
+
+   const [name,setName]=useState("");
+   const [date,setDate]=useState("");
+   const [location,setLocation]=useState("");
+   const [type,setType]=useState("");
+
+     const handleBooking=async()=>{
+       const service={Name:name,Price:price,address:location,date:date,animalType:type};
+       console.log(service)
+       
+       try {
+        await  axios.post("https://dark-pink-rabbit-wear.cyclic.cloud/service/register",service)
+                .then((res)=>{
+                  console.log(res)
+                })
+                .catch((err)=>console.log(err))
+      } catch (error) {
+            console.log(error)
+      }
+
+           
+       
+          
+     }
+ 
+  
+
 
 
   return (
@@ -59,7 +94,7 @@ const Service_cart = ({title,id,image,details,price}) => {
                     <ModalBody pb={6}>
                       <FormControl>
                         <FormLabel>Full name</FormLabel>
-                        <Input ref={initialRef} placeholder='Full name' />
+                        <Input ref={initialRef} placeholder='Full name' onChange={(e)=>setName(e.target.value)} />
                       </FormControl>
 
                       <FormControl mt={4}>
@@ -68,17 +103,18 @@ const Service_cart = ({title,id,image,details,price}) => {
                           placeholder="Select Date and Time"
                           size="md"
                           type="datetime-local"
+                          onChange={(e)=>setDate(e.target.value)}
                           />
                       </FormControl>
 
                       <FormControl mt={4}>
                         <FormLabel>Location</FormLabel>
-                        <Input placeholder="location" />
+                        <Input placeholder="location" onChange={(e)=>setLocation(e.target.value)} />
                       </FormControl>
 
                       <FormControl mt={4}>
                         <FormLabel>Animale Type</FormLabel>
-                        <Input placeholder="Animal Type" />
+                        <Input placeholder="Animal Type" onChange={(e)=>setType(e.target.value)} />
                       </FormControl>
 
                       <FormControl mt={4}>
@@ -89,8 +125,8 @@ const Service_cart = ({title,id,image,details,price}) => {
                     </ModalBody>
 
                     <ModalFooter>
-                      <Button colorScheme='blue' mr={3}>
-                        Save
+                      <Button onClick={handleBooking} colorScheme='blue' mr={3}>
+                        process to book
                       </Button>
                       <Button onClick={onClose}>Cancel</Button>
                     </ModalFooter>
